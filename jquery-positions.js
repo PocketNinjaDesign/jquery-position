e2c17d21ea928a68b0cf637be740c21082722da2
eyeamaman <duncan@we7.com>
2015-01-26 16:31:48 +0000

    Refactor adding x and y

--- jquery-positions.js
+++ jquery-positions.js
@@ -67,6 +67,29 @@
 ;(function() {
 
   var
+  
+    isObject = function(arg) {
+      var response = true;
+      
+      for (var i = 0; i < arguments.length; i++) {
+        if ( typeof arguments[i] !== 'object' ) {
+          response = false;
+        }
+      }
+      return response;
+    },
+    
+    isNumber = function(arg) {
+      var response = true;
+      
+      for (var i = 0; i < arguments.length; i++) {
+        if ( typeof arguments[i] !== 'number' ) {
+          response = false;
+        }
+      }
+      return response;
+    },
+  
     getXY = function(_direction, _type) {
       switch(_type) {
         case "abs":
@@ -81,50 +104,124 @@
       }
     },
   
-    xyPosition = function(_direction, _options) {
+    xyPosition = function(_options) {
       var
         opt = $.extend({
-          val: (isNaN(_options) ? null : _options)
+          val: (isNaN(_options) ? null : _options),
+          x: null,
+          y: null,
+          directions: null,
+          stepDuration: 0,
+          duration: 0,
+          fn: $.noop()
         }, _options, {});
 
-      switch(typeof _options) {
-        case "number":
-          $(this).animate({
-            top: (_direction === 'top')? _options: getXY.call(this, 'top'),
-            left: (_direction === 'left')? _options: getXY.call(this, 'left')
-          }, 0);
-          break;
-        case "string":
-          return getXY.call(this, _direction, _options);
-          break;
-        default:
-          return getXY.call(this, _direction);
-      }
+      for(var i = 0; i < this.length; i++) {
 
+        if (opt.direction === 'both') {
+          // Both x and y
+
+          setTimeout(function(_my) {
+            $(_my).animate({
+              left: opt.x,
+              top: opt.y
+            }, opt.duration, opt.fn);
+          }(this[i]), (opt.stepDuration * i) );
+          console.log('stepDuration:' + opt.stepDuration * i);
+        }
+        else {
+          // Just one direction
+          
+          switch(typeof _options) {
+            case "number":
+              $(this[i]).animate({
+                left: (opt.direction === 'left')? _options: getXY.call(this[i], 'left'),
+                top: (opt.direction === 'top')? _options: getXY.call(this[i], 'top')
+              }, opt.duration, opt.fn);
+              break;
+            case "string":
+              return getXY.call(this[i], opt.direction, _options);
+              break;
+            default:
+              return getXY.call(this[i], opt.direction);
+          }
+        }
+      }
+      
       return this;
     };
 
   $.fn.x = function(options) {
-    return xyPosition.call(this, 'left', options);
+    if ( isObject.apply(this, options) ) {
+      options.direction = 'left';
+    }
+    return xyPosition.call(this, options);
   }
-  
+
   $.fn.y = function(options) {
-    return xyPosition.call(this, 'top', options);
+    if ( isObject.apply(this, options) ) {
+      options.direction = 'top';
+    }
+    return xyPosition.call(this, options);
   };
 
   $.fn.xy = function(options) {
-    // options:
-    // {left:0, top:0}, string, num
-    // Additonal : {} - animation settings
-    
-    switch(typeof options) {
-      case "string":
-        return getXY.call(this, 'all', options + '-all');
-        break;
-      default:
-        return getXY.call(this, 'all', 'rel-all');
+    var opt;
+
+    if (arguments.length === 1) {
+      opt = options;
+      switch(typeof opt) {
+        case "string":
+          return getXY.call(this, 'all', opt + '-all');
+          break;
+        default:
+          return getXY.call(this, 'all', 'rel-all');
+      }
     }
+    else if(arguments.length === 2) {
+      if (isNumber.apply(this, arguments)) {
+        xyPosition.call(this, $.extend({
+          x: arguments[0],
+          y: arguments[1],
+          direction: 'both'
+        }));
+      }
+    }
+    else if (arguments.length === 3) {
+      if ( isNumber.apply(this, [arguments[0], arguments[1]]) && isObject.apply(this, arguments[2]) ) {
+        xyPosition.call(this, $.extend({
+            x: arguments[0],
+            y: arguments[1],
+            direction: 'both'
+          }, arguments[2]));
+      }
+    }
+
     return this;
   }
 })();
 
+
+/*
+
+$element.x();
+$element.x(10);
+$element.x('abs');
+$element.x({
+  val: 10,
+  duration: 0,
+  stepDuration: 0,
+  fn: $.noop()
+});
+
+
+$element.xy();
+$element.xy('abs');
+$element.xy(10, 100);
+$element.xy(10, 100, {
+  duration: 0,
+  stepDuration: 0,
+  fn: $.noop()
+});
+
+*/
